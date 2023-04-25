942acf75c882f45e204656723f9edf5bfb6ae523
Yaakov Yehezkel <YehezkelY@aetna.com>
2023-04-25 15:27:30 -0400

    updating init to make is smarter (thanks bing!)

--- base/init.sh
+++ base/init.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null 2>&1
+# Add Homebrew executable to path if it doesn't exist
+if ! grep -qxF 'export PATH="/usr/local/bin:$PATH"' ~/.bash_profile; then
+    echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile
+fi
+
+# Update Homebrew if it's already installed, otherwise install it
+if command -v brew >/dev/null 2>&1; then
+    brew update
+else
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Test if brew file exists before ln command
+if [ ! -f "/opt/homebrew/bin/brew" ]; then
+    echo "Error: Homebrew executable not found."
+    exit 1
+fi
+
+sudo ln -s /opt/homebrew/bin/brew /usr/local/bin/
 
 # Install brew formulas
-brew install --formula $(cat brew-formulas.list) --quiet > /dev/null 2>&1
+brew install --formula $(cat brew-formulas.list) --quiet
 
 # Install brew cask formulas
-brew install --cask $(cat brew-cask-formulas.list) --quiet > /dev/null 2>&1
+brew install --cask $(cat brew-cask-formulas.list) --quiet
 
 # Install Visual Studio Code extensions
-cat vscode-plugins | xargs -n 1 code --install-extension --force --silent > /dev/null 2>&1
+cat vscode-plugins | xargs -n 1 code --install-extension --force --silent
