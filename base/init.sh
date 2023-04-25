09e8edde07033ecf1ab68d8ea5a29bcb2dc47702
Yaakov Yehezkel <YehezkelY@aetna.com>
2023-04-25 16:28:32 -0400

    remove enpass from cask list update extensions install command

--- base/init.sh
+++ base/init.sh
@@ -28,4 +28,4 @@ brew install --formula $(cat brew-formulas.list) --quiet
 brew install --cask $(cat brew-cask-formulas.list) --quiet
 
 # Install Visual Studio Code extensions
-cat vscode-plugins | xargs -n 1 code --install-extension --force --silent
+cat vscode-plugins.list | xargs -n 1 code --force --install-extension
