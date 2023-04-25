bc826bb52aecc30f7c8f3dd12e086da8a4ba3635
Yaakov Yehezkel <YehezkelY@aetna.com>
2023-04-25 15:15:00 -0400

    allowing an unattended installation

--- base/init.sh
+++ base/init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null 2>&1
 
 # Install brew formulas
-brew install $(cat brew-formulas.list)
+brew install --formula $(cat brew-formulas.list) --quiet > /dev/null 2>&1
 
 # Install brew cask formulas
-brew install --cask $(cat brew-cask-formulas.list)
+brew install --cask $(cat brew-cask-formulas.list) --quiet > /dev/null 2>&1
 
 # Install Visual Studio Code extensions
-cat vscode-plugins | xargs -n 1 code --install-extension
+cat vscode-plugins | xargs -n 1 code --install-extension --force --silent > /dev/null 2>&1
